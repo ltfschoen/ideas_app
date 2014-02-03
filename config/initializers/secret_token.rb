7b0bbca54badb47b9fa06d6233f7af93656ce384
ltfschoen <ltfschoen@gmail.com>
2014-02-03 17:05:25 +1100

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdeasApp::Application.config.secret_key_base = 'dba3950079801de25e13fc0d74d557d1f166e300b69b39a7db5dcc5188b8aeb7d967c79d0271951e5602739a031b34ffb9d94e67cd263bce25cc6a0f4aec6df6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IdeasApp::Application.config.session_store :cookie_store, key: '_ideas_app_session'
