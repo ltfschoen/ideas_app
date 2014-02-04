478961a0e7f83382b742fa4175c8527ae188f4c7
ltfschoen <ltfschoen@gmail.com>
2014-02-04 12:55:56 +1100

    add category data from a .csv file migration working

---
+++ db/migrate/20140203065218_add_category_data.rb
@@ -0,0 +1,21 @@
+require 'fileutils'
+require 'csv'
+
+class AddCategoryData < ActiveRecord::Migration
+  def up
+
+		filename_new = "categories"
+
+		if File.file?("/Users/Luke/Documents/w5d1_project/ideas_app/db/#{filename_new}.csv")
+			CSV.foreach("/Users/Luke/Documents/w5d1_project/ideas_app/db/#{filename_new}.csv") do |col|
+				Category.create(:name => col[0].to_s, :description => col[1].to_s)
+			end
+		else 
+			raise "No file exists with name: #{filename_new}"
+		end
+  end
+
+  def down
+  	Category.all.destroy # only with team permission
+  end
+end
